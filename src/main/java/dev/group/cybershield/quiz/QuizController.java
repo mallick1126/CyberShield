4cd381bb3dac8d44ffb6475209620b6eb573b955
mallick1126 <rahulmallick1126@gmail.com>
2024-11-28 00:05:41 +0530

    controller changes

--- src/main/java/dev/group/cybershield/quiz/QuizController.java
+++ src/main/java/dev/group/cybershield/quiz/QuizController.java
@@ -2,22 +2,26 @@ package dev.group.cybershield.quiz;
 
 import dev.group.cybershield.entity.Questions;
 import dev.group.cybershield.repository.QuestionRepo;
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
 @Slf4j
 @RestController
+@Transactional
+@RequestMapping("/quiz")
 public class QuizController {
 
     @Autowired
     private QuestionRepo questionRepo;
 
-    @GetMapping("/testing")
-    public Questions testController(){
+    @GetMapping("/v1.0/getQuestions")
+    public Questions getQuestions(){
         Optional<Questions> questionData = questionRepo.findById(1L);
         log.info("fetched data from database " + questionData.get());
         return questionData.get();
